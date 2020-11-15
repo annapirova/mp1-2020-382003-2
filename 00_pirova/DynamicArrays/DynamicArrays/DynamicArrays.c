9f21ae449659d72269abf3fd5b5bb15219dcaf9f
annapirova <anna.yu.malova@gmail.com>
2020-11-16 02:13:01 +0300

    добавлены базовые операции с массивами

--- 00_pirova/DynamicArrays/DynamicArrays/DynamicArrays.c
+++ 00_pirova/DynamicArrays/DynamicArrays/DynamicArrays.c
@@ -1,5 +1,35 @@
+#include "stdio.h"
+#include "memory.h"
 
+void myMalloc2(int** B, int n)
+{
+	*B = (int*)malloc(sizeof(int) * n); // это адрес
+}
+
+int* myMalloc(int n)
+{
+
+	int* B2 = (int*)malloc(sizeof(int) * n);
+	return B2;
+}
+
+void print(int* B, int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		printf("%d ", B[i]);
+	printf("\n");
+}
+
+void randArray(int B[], int n, int a, int b)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		B[i] = rand() % (b - a) + a;
+}
 
 void main()
 {
+	int* B;
+	int n = 5;
 }
