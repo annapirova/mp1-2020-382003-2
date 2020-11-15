8d969e2d65f4a226782731272b59ab6a0f49d8ed
annapirova <anna.yu.malova@gmail.com>
2020-11-16 02:05:24 +0300

    empty roject

--- 00_pirova/Sort/Sort/Sort.c
+++ 00_pirova/Sort/Sort/Sort.c
@@ -1,194 +1,291 @@
-﻿#include "stdio.h"
+﻿#include "stdbool.h"
+#include "stdio.h"
 #include "stdlib.h"
-#include "stdbool.h"
 
-
-void print(int B[], int n);
-
-
-void BubbleSort(int A[], int n)
+void swaplementm(int B[], int i)
+{
+	int t;
+	t = B[i];
+	B[i] = B[i - 1];
+	B[i - 1] = t;
+}
+void swaplementp(int B[], int j)
+{
+	int t;
+	t = B[j];
+	B[j] = B[j + 1];
+	B[j + 1] = t;
+}
+void Vvodmassiv(int B[], int n)
+{
+	printf("Vvodite svoi massive:");
+	for (int i = 0; i < n; i++)
+	{
+		scanf_s("%d", &B[i]);
+	}
+}
+void Vivodmassiv(int B[], int n)
+{
+	printf("Vash massive:");
+	for (int i = 0; i < n; i++)
+	{
+		printf(" %d ", B[i]);
+	}
+	printf("\n");
+}
+void Randmassiv(int B[], int n)
+{
+	int max = 100;
+	int min = 50;
+	int proverka;
+	printf("Vvedite max and min,or oni budut vibrani 100 and 50\n");
+	printf("If vi soglasni s max and min, napishite 1, else, napishite 2\n");
+	scanf_s("%d", &proverka);
+	switch (proverka)
+	{
+	case 1: {
+		printf("max:%d min:%d", max, min);
+		int i;
+		for (i = 0; i < n; i++)
+			B[i] = rand() % (max - min) + min;
+		break;
+	}
+	case 2: {
+		printf("Vvedite svoi max and min:");
+		scanf_s("%d %d", &max, &min);
+		printf("max:%d min:%d", max, min);
+		int i;
+		for (i = 0; i < n; i++)
+			B[i] = rand() % (max - min) + min;
+		break;
+	}
+	}
+}
+void BubbleSort(int B[], int n)
 {
 	int i, j;
-	int tmp;
-	bool wasSwap;
-
+	bool wasSwap = false;
 	for (i = 0; i < n; i++)
 	{
 		wasSwap = false;
 		for (j = 0; j < n - i - 1; j++)
 		{
-			if (A[j] > A[j + 1])
+			if (B[j] > B[j + 1])
 			{
-				tmp = A[j];
-				A[j] = A[j + 1];
-				A[j + 1] = tmp;
+				swaplementp(B, j);
+				//tmp = B[j];
+				//B[j] = B[j + 1];
+				//B[j + 1] = tmp;
 				wasSwap = true;
 			}
 		}
-		if (!wasSwap) // wasSwap == false
+		if (!wasSwap)
 		{
 			break;
 		}
+	}
+}
+void QuickSort(int B[], int n)
+{
+	int first = 0;
+	int last = n - 1;
+	if (first < last)
+	{
+		int left = first, right = last, middle = B[(left + right) / 2];
+		do
+		{
+			while (B[left] < middle) left++;
+			while (B[right] > middle) right--;
+			if (left <= right)
+			{
+				int tmp = B[left];
+				B[left] = B[right];
+				B[right] = tmp;
+				left++;
+				right--;
+			}
+		} while (left <= right);
+		QuickSort(B, first, right);
+		QuickSort(B, left, last);
+	}
+}
+void InsertSort(int B[], int n)
+{
+	int Elementvzat, index;
 
-		print(A, n);
+	for (int i = 1; i < n; i++)
+	{
+		Elementvzat = B[i];
+
+		index = i - 1;
+		//printf("\n Elementvzat: %d \n i=%d  \n index=%d \n", Elementvzat, i, index);
+		while (index >= 0 && B[index] > Elementvzat)
+		{
+			B[index + 1] = B[index];
+			index = index - 1;
+		}
+		B[index + 1] = Elementvzat;
+		//printf("\n");
+		/*for (int i = 0; i < n; i++)
+		{
+			printf(" %d ", B[i]);
+		}*/
 	}
 
-	printf("i = %d\n", i);
 }
-
-
-void randArray(int B[], int n, int a, int b)
+void ShakerSort(int B[], int n)
 {
-	int i;
-	for (i = 0; i < n; i++)
-		B[i] = rand() % (b - a) + a;
-}
+	int left = 1;
+	int right = n - 1;
+	while (left <= right)
+	{
+		for (int i = right; i >= left; i--)
+			if (B[i - 1] > B[i]) swaplementm(B, i);
+		left++;
 
-void sortedArray(int B[], int n)
+		for (int i = left; i <= right; i++)
+			if (B[i - 1] > B[i]) swaplementm(B, i);
+		right--;
+
+	}
+}
+void ShellSort(int B[], int n)
 {
-	int i; int tmp;
-	for (i = 0; i < n; i++)
-		B[i] = i;
-
-	tmp = B[n - 2];
-	B[n - 2] = B[n - 1];
-	B[n - 1] = tmp;
+	int i, j, step;
+	int tmp;
+	for (step = n / 2; step > 0; step /= 2)
+		for (i = step; i < n; i++)
+		{
+			tmp = B[i];
+			for (j = i; j >= step; j -= step)
+			{
+				if (tmp < B[j - step])
+					B[j] = B[j - step];
+				else
+					break;
+			}
+			B[j] = tmp;
+		}
+}
+void SlianSort(int B[], int lowIndex, int highIndex) {
+	if (lowIndex == highIndex)
+		return;
+	else {
+		int midIndex = (lowIndex + highIndex) / 2;
+		SlianSort(B, lowIndex, midIndex);
+		SlianSort(B, midIndex + 1, highIndex);
+		SlianSort(B, lowIndex, midIndex, highIndex);
+	}
 }
 
-void print(int B[], int n)
-{
-	int i;
-	for (i = 0; i < n; i++)
-		printf("%d ", B[i]);
-	printf("\n");
-}
 
 void menu()
 {
 	printf("\nMENU:\n");
-	printf("1. Input array\n");
-	printf("2. Print array\n");
-	printf("3. BubbleSort\n");
-	printf("4. QuickSort\n");
-	printf("5. InsertSort\n");
-	printf("6. MergeSort\n");
+	printf("1. Vvesti massive\n");
+	printf("2. Vosproizvesti massive\n");
+	printf("3. Randomny massive\n");
+	printf("4. Bubble Sortirovka\n");
+	printf("5. Quick Sortirovka (Harra)\n");
+	printf("6. Insert Sortirovka\n");
+	printf("7. Shaker Soritrovka\n");
+	printf("8. Shell Sortirovka\n");
+	printf("9. Sliyanie Sortirovka\n");
 	printf("0. Exit\n");
 }
 
-void Split(int B[], int left, int right)
-{
-	int mid, i, j, tmp;
-
-	mid = B[(right + left) / 2];
-	i = left;
-	j = right;
-
-	//printf("mid %d left %d right %d\n", mid, left, right);
-
-	while (i < j)
-	{
-		while (B[i] < mid)
-		{
-			i++;
-		}
-		while (B[j] > mid) 
-		{
-			j--;
-		}
-
-		if (i <= j) // <=
-		{
-			tmp = B[i];
-			B[i] = B[j];
-			B[j] = tmp;
-			i++;
-			j--;
-		}
-	}
-	 
-	//printf("=> i %d j %d\n", i, j);
-
-	// половинка от left до i
-	if (left < j)
-		Split(B, left, j);
-	// половинка от j до right
-	if (i < right)
-		Split(B, i, right);
-}
-
-void QuickSort(int B[], int n)
-{
-	Split(B, 0, n - 1);
-}
-
-int Check(int B[], int n)
-{
-	bool f = true;
-	int i;
-
-	for (i = 0; i < n - 1; i++)
-	{
-		if (B[i + 1] < B[i])
-		{
-			f = false;
-			break;
-		}
-	}
-	return f;
-}
-
-
 void main()
 {
-	int B[100];
-	int n = 100;
+	int B[10];
+	int n = 10;
 	int t = 10;
-	bool wasInput = true;
-
-	printf("start\n");
-	srand(1000);
-
+	bool wasInput = false;
 	while (t != 0)
 	{
 		menu();
 		scanf_s("%d", &t);
 		switch (t)
 		{
-		case 1: {
-//			sortedArray(B, n);
-			randArray(B, n, -10, 10); 
+		case 1:
+		{
+			Vvodmassiv(B, n);
 			wasInput = true;
 			break;
 		}
-		case 2: {
-			if (wasInput)
-				print(B, n);
-			else printf("Please, input array\n");
+		case 2:
+		{
+			if (wasInput) {
+				Vivodmassiv(B, n);
+			}
+			else printf("Vvedi massive blen\n");
 			break;
 		}
-		case 3: {
-			if (wasInput == 1)
-			{
+		case 3:
+		{
+			Randmassiv(B, n);
+			wasInput = true;
+			break;
+		}
+		case 4:
+		{
+			if (wasInput) {
+
 				BubbleSort(B, n);
-				print(B, n);
+				Vivodmassiv(B, n);
 			}
-			else printf("Please, input array\n");
+			else printf("Vvedi massive blen\n");
 			break;
 		}
-		case 4: {
-			if (wasInput == 1)
-			{
+		case 5:
+		{
+			if (wasInput) {
+
 				QuickSort(B, n);
-				if (Check(B, n))
-					printf("correct\n");
-				else
-					printf("error\n");
-				print(B, n);
+				Vivodmassiv(B, n);
 			}
-			else printf("Please, input array\n");
+			else printf("Vvedi massive blen\n");
+			break;
+		}
+		case 6:
+		{
+			if (wasInput) {
+
+				InsertSort(B, n);
+				Vivodmassiv(B, n);
+			}
+			else printf("Vvedi massive blen\n");
+			break;
+		}
+		case 7:
+		{
+			if (wasInput) {
+
+				ShakerSort(B, n);
+				Vivodmassiv(B, n);
+			}
+			else printf("Vvedi massive blen\n");
+			break;
+		}
+		case 8:
+		{
+			if (wasInput) {
+
+				ShellSort(B, n);
+				Vivodmassiv(B, n);
+			}
+			else printf("Vvedi massive blen\n");
+			break;
+		}
+		case 9:
+		{
+			if (wasInput) {
+
+				SlianSort(B, 0, n - 1);
+				Vivodmassiv(B, n);
+			}
+			else printf("Vvedi massive blen\n");
 			break;
 		}
 		}
 	}
 }
-
