b759d3e75698121f4147393241777b1727fa8c41
annapirova <anna.yu.malova@gmail.com>
2020-11-24 11:12:56 +0300

    работа с одномерным двумерным массивом

--- 00_pirova/2dArray/2dArray/2dArray/2dArrays.c
+++ 00_pirova/2dArray/2dArray/2dArray/2dArrays.c
@@ -16,8 +16,51 @@ void print(int* B, int n)
 	printf("\n");
 }
 
+// A[i, j] = A[i, j] + A2[i, j]
+void MatPlusEq(int* A, int* A2, int n, int m)
+{
+	int size = n * m;
+	int i;
+	for (i = 0; i < size; i++)
+		A[i] += A2[i];
+}
+
+// A[i, j] = A[i, j] + A2[i, j]
+void MatPlusEq2(int* A, int* A2, int n, int m)
+{
+	int i, j;
+	// [i, j] --> [i*m + j]
+	for (i = 0; i < n; i++)
+		for (j = 0; j < m; j++)
+			A[i * m + j] += A2[i * m + j];
+}
+
+void Print2D(int* B, int n, int m)
+{
+	int i;
+	for (i = 0; i < n; i++)
+		print(&(B[i * m]), m);
+	printf("\n");
+}
+
 void main()
 {
-	int** B;
-	int n, m;
+	int* B, *B2;
+	int n = 5, m = 3;
+	int i, j;
+	// матрица размера n * m
+	B = (int*)malloc(sizeof(int) * n * m);
+	B2 = (int*)malloc(sizeof(int) * n * m);
+	randArray(B, n * m, -5, 30);
+	randArray(B2, n * m, 2, 17);
+	// [i, j] --> [i*m + j]
+	// 0я строка с 0 по m - 1 инд
+	//1я строка с m по m + m - 1 = 2m-1
+	Print2D(B, n, m);
+	Print2D(B2, n, m);
+	MatPlusEq2(B, B2, n, m);
+	Print2D(B, n, m);
+
+	free(B);
+	free(B2);
 }
