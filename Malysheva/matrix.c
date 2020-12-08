bb7e19321ef7c8178a746b3a510526d5401413d3
Elena <roxy.nnov@gmail.com>
2020-12-08 17:45:38 +0300

    add simple matrix task 11

---
+++ Malysheva/matrix.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include "locale.h"
+
+void carray(int a[][10], int n, int m)
+{ 
+    int i, j;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        a[i][j] = rand() % 1000;
+    }
+}
+
+void trans(int a[][10], int n, int m)
+{
+    int array[m][n], i, j;
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            array[i][j] = a[j][i];
+        }
+    }
+    for (i = 0; i < m; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            a[i][j] = array[i][j];
+        }
+    }
+}
+
+void print(int a[][10], int n, int m)
+{
+   for(int i = 0; i < n; i++)  
+   {
+     for(int j = 0; j < m; j++) 
+       {
+         printf("%d \t", a[i][j]);
+       }
+     printf("\n"); 
+   } 
+
+}
+
+void menu()
+{
+    printf("1. создать массив \n");
+    printf("2. транспортировать матрицу \n");
+    printf("3. вывести массив \n");
+}
+
+void main()
+{
+    setlocale(LC_ALL, "rus");
+    int t = 1, n, m, a[10][10], tmp;
+    srand(12223);
+    
+    while ((t == 1) || (t == 2) || (t == 3))
+    {
+    
+    menu();
+    scanf("%d", &t);
+    switch(t)
+    {
+        case 1:
+            n = 1 + rand() % 9;
+            m = 1 + rand() % 9;
+            carray(a, n, m);
+            break;
+        case 2:
+            trans(a, n, m);
+            tmp = n;
+            n = m;
+            m = tmp;
+            break;
+        case 3:
+            print(a, n, m);
+            break;
+        default:
+            break;
+    }
+    }
+    
+}
