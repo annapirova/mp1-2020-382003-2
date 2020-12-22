c2bb2a4c691b6c8ae8e5996d085b2177dfa9757f
Elena <roxy.nnov@gmail.com>
2020-12-22 23:05:20 +0300

    add string 1

---
+++ Malysheva/str1.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<locale.h>
+#include <string.h>
+
+char dl(char* str, int len)
+{
+    int i, max = 0, im = 0, count = 0;
+    for (i = 0; i < len; i++)
+    {
+        if (str[i] != ' ') 
+            count += 1;
+        else 
+        {
+            if (count > max) 
+            {
+                max = count;
+                im = i - count;
+            }
+            count = 0;
+        }
+    }
+
+    if (count > max) 
+    {
+        max = count;
+        im = i - count;
+    }
+    max += im;
+    for (i = im; i < max; i++)
+        printf("%c", str[i]);
+}
+
+void main()
+{
+    char str[128];
+    int len;
+    gets(str);
+    len = strlen(str);
+    dl(str, len);
+
+}
