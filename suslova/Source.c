1095926bacb81a7a4465af4e9c14cc52d0ac0ba3
Виктория Суслова <73444769+vikkkkos@users.noreply.github.com>
2020-12-23 13:59:22 +0300

    Add files via upload

---
+++ suslova/Source.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+#include "string.h"
+
+void main()
+{
+	char* str = "C:\\Users/User\OneDrive\\Рабочий стол/exam_part_1.pdf";
+	int len, slash, tochka;
+	len = strlen(str);
+	slash = len;
+	tochka = len;
+
+	while ((str[slash] != '/') && (str[slash] != '\\'))
+	{
+		slash--;
+	}
+	while (str[tochka] != '.')
+	{
+		tochka--;
+	}
+	for (len = slash; len < tochka; len++)
+		printf("%c", str[len]);
+}
