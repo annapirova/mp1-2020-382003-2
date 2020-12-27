fecf7e9854329b0e951944e360f86780448091f3
Temaboom <artemka14nn@gmail.com>
2020-12-28 00:05:08 +0300

    Исправил двумер. масс. легкую

--- Двум. масс. 1/Проект3/Source.c
+++ Двум. масс. 1/Проект3/Source.c
@@ -3,20 +3,21 @@
 #include <stdlib.h>
 void main()
 {
-	double *A;
+	double **A;
 	double aver;
 	int i, j, n, m;
 	printf("Input lines ");
 	scanf_s("%d", &n);
 	printf("Input columns ");
 	scanf_s("%d", &m);
-	A = (double*)malloc(n*m * sizeof(double));
+	A = (double**)malloc(n*sizeof(double*));
 	for (i = 0; i<n; i++)
 	{
+		A[i] = (double*)malloc(m * sizeof(double));
 		for (j = 0; j<m; j++) 
 		{
-			printf("a[%d][%d] = ", i, j);
-			scanf_s("%lf", (A + i*m + j));
+			printf("A[%d][%d] = ", i, j);
+			scanf_s("%lf", &A[i][j]);
 		}
 	}
 
@@ -24,18 +25,19 @@ void main()
 	{
 		aver = 0;
 		for (j = 0; j < m; j++)
-			aver = aver + *(A + i*m + j);
+			aver = aver + A[i][j];
 		aver = aver / m;
 		for (j = 0; j < m; j++)
-			*(A + i*m + j) = *(A + i*m + j) - aver;
+			A[i][j] = A[i][j] - aver;
 	}
 	for (i = 0; i<n; i++) 
 	{
 		for (j = 0; j<m; j++) 
 		{
-			printf("%lf ", *(A + i*m + j)); 
+			printf("%lf ", A[i][j]); 
 		}
 		printf("\n");
+		free(A[i]);
 	}
 	free(A);
 	scanf_s("%d", &i);
