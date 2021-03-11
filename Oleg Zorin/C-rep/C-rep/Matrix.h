56145a78be1278c3fcf0d61f79444ed1c10acd76
Oleg <ol3g.zor1n@yandex.ru>
2021-03-12 00:35:26 +0300

    Класс матриц + сырой метод Гаусса.

---
+++ Oleg Zorin/C-rep/C-rep/Matrix.h
@@ -0,0 +1,24 @@
+#include <iostream>
+
+class matrix
+{
+	double** A;
+	int n, m;
+public:
+	matrix();
+	matrix(int n, int m);
+	matrix(const matrix& other);
+	~matrix();
+
+	matrix& operator=(const matrix& other);
+	matrix operator+(const matrix& other);
+	matrix operator-(const matrix& other);
+	matrix operator*(const matrix& other);
+	matrix& operator+=(const matrix& other);
+	matrix& operator-=(const matrix& other);
+	friend matrix operator*(const matrix& other, double p);
+	friend matrix operator*(double p, const matrix& other);
+	friend std::ostream& operator<<(std::ostream& os, const matrix& mat);
+	double* operator[](int i);
+	void Gauss();
+};
--- Oleg Zorin/C-rep/C-rep/main.cpp
+++ Oleg Zorin/C-rep/C-rep/main.cpp
@@ -1,53 +1,19 @@
-#include "triangle.h"
-#include "Complex.h"
+#include "Matrix.h"
 #include <iostream>
 
 using namespace std;
 
-void main1()
-{
-	Triangle t1, t2(2.3, 3.2, 1.5), t3(t2);
-
-	double per1 = t1.GetPer();
-	double per2 = t2.GetPer();
-	double per3 = t3.GetPer();
-	double sqr2 = t2.GetSqr();
-	double sqr3 = t3.GetSqr();
-	double sqr1 = t1.GetSqr();
-
-	cout << "Perimeter:\n" << per1 << endl << per2 << endl << per3 << endl;
-	cout << "__________________________________________________________" << endl;
-	cout << "Square:\n" << sqr1 << endl << sqr2 << endl << sqr3 << endl;
-	cout << "__________________________________________________________" << endl;
-	
-	t1.~Triangle();
-	t2.~Triangle();
-	t3.~Triangle();
-}
-
 int main()
 {
-	Complex t, a(1, 1), b(2, 3), c, d;
-
-	c = a + b;
-	c.Print();
-	cout << endl << endl;
-	c = a - b;
-	c.Print();
-	cout << endl << endl;
-	c = a * b;
-	c.Print();
-	cout << endl << endl;
-	c = a / b;
-	c.Print();
-	cout << endl << endl;
-	t = d = b;
-	t.Print();
-	cout << endl << endl;
-	d.Print();
+	matrix a(100, 100);
+	matrix b(1, 2);
 
 
+	cout << "Standart:\n" << a << endl;
+	a.Gauss();
+	cout.precision(2);
+	cout << "After Gauss:\n" << a << endl;
 
-
+	cout << "end" << endl;
 	return 0;
 }
