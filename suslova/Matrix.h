b744d89d92acabbf885a7187f0676ab8da6f82f7
Виктория Суслова <73444769+vikkkkos@users.noreply.github.com>
2021-05-19 09:50:41 +0300

    Update Matrix.h

--- suslova/Matrix.h
+++ suslova/Matrix.h
@@ -13,12 +13,13 @@ public:
     Matrix(const Matrix& B);
     ~Matrix();
 
-    Matrix operator+(const Matrix& B) const;
-    Matrix operator-(const Matrix& B) const;
+   Matrix operator+(const Matrix& B) const;
+    Matrix& operator-(const Matrix& B) const;
     Matrix operator*(const Matrix& B) const;
     Matrix operator*(double A) const;
     friend Matrix operator*(double A, const Matrix& B);
-    Matrix& operator+=(const Matrix& B);
+    Matrix operator*(double var);
+    Matrix operator+=(const Matrix& B);
     Matrix& operator-=(const Matrix& B);
     Matrix& operator=(const Matrix& B);
     double* operator[](int pos);
@@ -26,6 +27,6 @@ public:
     friend std::istream& operator>>(std::istream& is, Matrix& B);
     friend class Vector;
     friend class Gauss;
-
+    
     void InputMatr(double** A, int n, int m);
 };
